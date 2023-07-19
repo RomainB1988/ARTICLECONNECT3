de3667adb53eb45a9858747c5ffa3d01d398f7bd
Romain Buisine rom.buisine@gmail.com <rom.buisine@gmail.com>
2023-07-19 16:23:56 +0200

    setup

--- config.ru
+++ config.ru
@@ -4,3 +4,10 @@ require_relative "config/environment"
 
 run Rails.application
 Rails.application.load_server
+Rails.application.routes.draw do
+  root to: 'home#index'
+
+  resources :posts, only: [:index, :show] do
+    resources :comments, only: [:create]
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'comments/create'
+  get 'posts/index'
+  get 'posts/show'
+  get 'home/index'
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
